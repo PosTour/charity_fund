cbe0e53a2452fd7ec9c0cc1309dc76445ca5a04e
posTour <dimidov496@gmail.com>
2025-01-10 14:24:24 +0300

    feat: созданы dto для получения данных пользователей при регистрации

---
+++ src/main/java/ru/bmstu/charity/dto/ClientRegistrationDto.java
@@ -0,0 +1,33 @@
+package ru.bmstu.charity.dto;
+
+import jakarta.validation.constraints.*;
+
+public class ClientRegistrationDto {
+
+    @NotEmpty(message = "Поле логина должно быть заполнено")
+    @Size(min = 2, max = 100, message = "Логин содержать от 5 до 100 символов")
+    private String username;
+
+    @NotEmpty
+    @NotNull
+    private String password;
+
+    @NotEmpty
+    @NotNull
+    private String role;
+
+    @Pattern(regexp = "^[А-ЯЁ][a-яё]+ [А-ЯЁ][a-яё]+ [А-ЯЁ][a-яё]+$",
+            message = "Полное имя имеет следующий вид: 'Фамилия Имя Отчество'")
+    private String name;
+
+    @NotEmpty
+    @NotNull
+    private String address;
+
+    @Pattern(regexp = "^\\d{11}$",
+            message = "Телефон записывается начинается с цифры 8 и содержит 11 символов")
+    private String phone;
+
+    @Email(message = "Некорректный адрес электронной почты")
+    private String email;
+}
---
+++ src/main/java/ru/bmstu/charity/dto/EmployeeRegistrationDto.java
@@ -0,0 +1,28 @@
+package ru.bmstu.charity.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class EmployeeRegistrationDto {
+
+    @NotEmpty(message = "Поле логина должно быть заполнено")
+    @Size(min = 2, max = 100, message = "Логин содержать от 5 до 100 символов")
+    private String username;
+
+    @NotEmpty
+    @NotNull
+    private String password;
+
+    @NotEmpty
+    private String role;
+
+    @Pattern(regexp = "^[А-ЯЁ][a-яё]+ [А-ЯЁ][a-яё]+ [А-ЯЁ][a-яё]+$",
+            message = "Полное имя имеет следующий вид: 'Фамилия Имя Отчество'")
+    private String name;
+
+    @NotEmpty
+    @NotNull
+    private String position;
+}
